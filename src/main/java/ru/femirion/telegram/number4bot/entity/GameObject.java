dd20dafc7b60fde6ee5374e30c1521ef056055dd
A.Vostryakov <femirion@gmail.com>
2021-02-10 00:38:11 +0300

    fix game entity

--- src/main/java/ru/femirion/telegram/number4bot/entity/GameObject.java
+++ src/main/java/ru/femirion/telegram/number4bot/entity/GameObject.java
@@ -1,9 +1,13 @@
 package ru.femirion.telegram.number4bot.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class GameObject {
   private String objectId;
--- src/main/java/ru/femirion/telegram/number4bot/entity/Player.java
+++ src/main/java/ru/femirion/telegram/number4bot/entity/Player.java
@@ -1,14 +1,18 @@
 package ru.femirion.telegram.number4bot.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Player {
   private String playerId;
   private String name;
   private String desc;
-  private List<GameObject> objects;
+  private List<String> objects;
   private List<String> actions;
 }
