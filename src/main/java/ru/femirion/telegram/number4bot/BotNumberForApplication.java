6d9a32db749a88a7a56f8ffd8445534e290ae7a2
A.Vostryakov <femirion@gmail.com>
2021-02-28 20:02:05 +0300

    fix scheduled task

--- src/main/java/ru/femirion/telegram/number4bot/BotNumberForApplication.java
+++ src/main/java/ru/femirion/telegram/number4bot/BotNumberForApplication.java
@@ -15,46 +15,45 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class BotNumberForApplication {
-    private static final Map<String, String> ENV = System.getenv();
-    private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();;
+  private static final Map<String, String> ENV = System.getenv();
+  private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+  ;
 
-    public static void main(String[] args) {
-        try {
-            var botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            var bot = new Bot(ENV.get("BOT_NAME"), ENV.get("TOKEN"));
-            botsApi.registerBot(bot);
-            log.info("start bot");
+  public static void main(String[] args) {
+    try {
+      var botsApi = new TelegramBotsApi(DefaultBotSession.class);
+      var bot = new Bot(ENV.get("BOT_NAME"), ENV.get("TOKEN"));
+      botsApi.registerBot(bot);
+      log.info("start bot");
 
-            var notificationThread = creatNotificationThread(bot);
-            executor.scheduleAtFixedRate(notificationThread, 5, 10, TimeUnit.SECONDS);
-        } catch (TelegramApiException ex) {
-            log.error("bot initialization error", ex);
+      var notificationThread = creatNotificationThread(bot);
+      executor.scheduleAtFixedRate(notificationThread, 5, 10, TimeUnit.SECONDS);
+    } catch (TelegramApiException ex) {
+      log.error("bot initialization error", ex);
+    }
+    executor.shutdown();
+  }
+
+  private static Runnable creatNotificationThread(Bot bot) {
+    return () -> {
+      log.info("start monitoring notification");
+      try {
+        var now = ZonedDateTime.now();
+        var players = Bot.getPlayers();
+        for (Player player : players) {
+          if (player.getTimeNextNotification() != null
+                  && player.getTimeNextNotification().isBefore(now)
+                  && player.getTextNextNotification() != null) {
+            bot.sendToPlayer(player.getChatId(), player.getName(), player.getTextNextNotification());
+            log.info("send notification to player={}, text={}", player.getName(), player.getTextNextNotification());
+            player.setTimeNextNotification(null);
+            player.setTextNextNotification(null);
+          }
         }
-        executor.shutdown();
-    }
-
-    private static Runnable creatNotificationThread(Bot bot) {
-        return () -> {
-            log.info("start monitoring notification");
-            while (!executor.isShutdown()) {
-                try {
-                    var now = ZonedDateTime.now();
-                    var players = Bot.getPlayers();
-                    for (Player player : players) {
-                        if (player.getTimeNextNotification() != null
-                                && player.getTimeNextNotification().isBefore(now)
-                                && player.getTextNextNotification() != null) {
-                            bot.sendToPlayer(player.getChatId(), player.getName(), player.getTextNextNotification());
-                            log.info("send notification to player={}, text={}", player.getName(), player.getTextNextNotification());
-                            player.setTimeNextNotification(null);
-                            player.setTextNextNotification(null);
-                        }
-                    }
-                } catch (Exception ex) {
-                    log.error("error in notification thread", ex);
-                }
-            }
-            log.info("end monitoring notification");
-        };
-    }
+      } catch (Exception ex) {
+        log.error("error in notification thread", ex);
+      }
+      log.info("end monitoring notification");
+    };
+  }
 }
